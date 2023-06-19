6d4ce4e17c328383ff6559ae51f630453f10a2ec
ct-atf <cau.ta@artatechfin.com>
2023-06-19 17:35:14 +0700

    airdrop function

--- js/src/util/new-account-with-lamports.ts
+++ js/src/util/new-account-with-lamports.ts
@@ -1,4 +1,4 @@
-import {Keypair, Connection} from '@solana/web3.js';
+import {Keypair, Connection, PublicKey} from '@solana/web3.js';
 
 import {sleep} from './sleep';
 
@@ -10,6 +10,26 @@ export async function newAccountWithLamports(
 
   let retries = 30;
   await connection.requestAirdrop(account.publicKey, lamports);
+  for (;;) {
+    await sleep(500);
+    if (lamports == (await connection.getBalance(account.publicKey))) {
+      return account;
+    }
+    if (--retries <= 0) {
+      break;
+    }
+  }
+  throw new Error(`Airdrop of ${lamports} failed`);
+}
+
+export async function requestSolToken(connection: Connection,
+  address: PublicKey,
+  lamports: number = 1000000,
+): Promise<Keypair> {
+  const account = Keypair.generate();
+
+  let retries = 30;
+  await connection.requestAirdrop(address, lamports);
   for (;;) {
     await sleep(500);
     if (lamports == (await connection.getBalance(account.publicKey))) {
